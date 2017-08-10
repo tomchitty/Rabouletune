f318bad1539c9c03deef17611164db248fbe3ed5
Tom Chitty <stagiaire3.web@skazy.nc>
2017-08-11 08:36:42 +1100

    Base du projet

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stagiaire3.web
+ * Date: 11/08/2017
+ * Time: 08:35
+ */
+
+header("Location: login.php");
---
+++ login.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stagiaire3.web
+ * Date: 11/08/2017
+ * Time: 08:30
+ */
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Rabouletune</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <h1>Login</h1>
+</body>
+</html>
